b91cd94d6ae4dec404a4a8a7fda793cf091ae79e
santhosh-sundarasamy <57383545+santhosh-sundarasamy@users.noreply.github.com>
2019-12-28 12:17:39 +0530

    Initial Version

---
+++ bankStmtMgr/src/main/java/com/stmtmanager/bankstmt/BankStmtMgrApplication.java
@@ -0,0 +1,13 @@
+package com.stmtmanager.bankstmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStmtMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStmtMgrApplication.class, args);
+	}
+
+}
---
+++ bankStmtMgr/src/test/java/com/stmtmanager/bankstmt/BankStmtMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stmtmanager.bankstmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankStmtMgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
